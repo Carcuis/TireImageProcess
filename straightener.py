3f0300aa5075f70ea9b0ef7b28e649b9ca18b596
Carcuis <ctx1256@foxmail.com>
2023-04-15 22:20:33 +0800

    Update Straightener

--- straightener.py
+++ straightener.py
@@ -1,60 +1,82 @@
+import os
+import time
+
 import cv2
-import numpy as np
+from matplotlib import pyplot as plt
+
+from util import Util
 
 
 class Straightener:
     def __init__(self) -> None:
-        pass
+        self.save_path = Util.generate_new_save_path("straighten")
 
-    def straighten(self) -> None:
-        # 1. 预处理
-        img = cv2.imread("img\\20230412\\6\\20230412_112133.jpg")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    def straighten(self, img_path: str, preview: bool = True, save: bool = True) -> None:
+        """
+        读取图片进行拼接
 
-        # 2. 轮廓检测
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        :param img_path: 图片路径
+        :param preview: 是否预览
+        :param save: 是否保存
+        """
 
-        # 3. 筛选轮廓
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area < 1000:  # 根据实际情况设置面积阈值
-                continue
-            epsilon = 0.1 * cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, epsilon, True)
-            if len(approx) != 4 or not cv2.isContourConvex(approx):
-                continue
-            x, y, w, h = cv2.boundingRect(approx)
-            if w < h:
-                approx = approx.reshape((4, 2))
-                approx = approx[[1, 2, 3, 0]]
-            break
+        print(f"Straightening: {img_path}")
+        img = cv2.imread(img_path)
 
-        # 4. 扭曲校正
-        rect = cv2.minAreaRect(approx)
-        angle = rect[-1]
-        if angle < -45:
-            angle += 90
-        M = cv2.getRotationMatrix2D(rect[0], angle, 1.0)
-        img_rot = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]), flags=cv2.INTER_CUBIC)
+        # 找到轮廓
+        center = (img.shape[1] // 2, img.shape[0] + 750)
+        radius = int(img.shape[1] * 1.15)
+        # cv2.circle(img, center, radius, (0, 0, 255), 4)
 
-        # 5. 裁剪反投影
-        dst_size = (int(rect[1][0]), int(rect[1][1]))
-        dst_corners = np.array([[0, 0],
-                                [dst_size[0], 0],
-                                [dst_size[0], dst_size[1]],
-                                [0, dst_size[1]]], dtype=np.float32)
-        src_corners = cv2.boxPoints(rect).astype(np.float32)
-        M = cv2.getPerspectiveTransform(src_corners, dst_corners)
-        dst = cv2.warpPerspective(img_rot, M, dst_size)
+        polar = cv2.warpPolar(img, (10000, 10000), center, radius, cv2.WARP_FILL_OUTLIERS + cv2.WARP_POLAR_LOG)
+        polar_gray = cv2.cvtColor(polar, cv2.COLOR_BGR2GRAY)
+        # 找到非0部分的坐标
+        coords = cv2.findNonZero(polar_gray)
+        # 计算非0部分的矩形区域
+        x, y, w, h = cv2.boundingRect(coords)
+        # 裁剪图片并放大
+        crop = polar[y:y + h, x:x + w]
+        crop = cv2.rotate(crop, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        crop = crop[0:crop.shape[0] // 2, 0:crop.shape[1]]
+        # cv2.rectangle(result, (580, 0), (result.shape[1] - 580, result.shape[0]), (0, 0, 255), 4)
+        result = crop[0:crop.shape[0], 580:crop.shape[1] - 580]
 
-        # 6. 输出
-        cv2.imwrite("result.png", dst)
+        # 显示结果
+        if preview:
+            plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            plt.show()
+            plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+            plt.show()
+
+        if save:
+            file_name = f"{self.save_path}/straighten_{Util.get_file_name_without_ext(img_path)}.jpg"
+            print(f"Saving to {file_name}...")
+            cv2.imwrite(file_name, result)
+
+    def straighten_from_folder(self, img_folder: str, preview: bool = True, save: bool = True) -> None:
+        """
+        从文件夹中读取图片进行拼接
+
+        :param img_folder: 图片文件夹
+        :param preview: 是否预览
+        :param save: 是否保存
+        """
+
+        print(f"Straightening from folder: {img_folder}")
+        for img_name in os.listdir(img_folder):
+            img_path = os.path.join(img_folder, img_name)
+            start = time.time()
+            self.straighten(img_path, preview, save)
+            end = time.time()
+            print(f"--Time spent: {end - start}")
 
 
 def main():
     straightener = Straightener()
-    straightener.straighten()
+    start = time.time()
+    straightener.straighten_from_folder("img/20230412/5", preview=True, save=True)
+    end = time.time()
+    print("Total time spent: ", end - start)
 
 
 if __name__ == "__main__":
